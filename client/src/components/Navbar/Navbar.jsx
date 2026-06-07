e2367ab8beac404a546cdb172e71161d42c0797b
Jaden Humphries <jaden.humphries@gmail.com>
2026-06-07 15:11:50 -0400

    updated Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div
-                className="app-name-icon-container"
+                className="app-icon-container"
                 role="button"
                 tabIndex={0}
                 aria-label="Go to home page"
@@ -20,12 +20,22 @@ function Navbar() {
                 }}
             />
 
-            <h2>JobSearch AI</h2>
+            <div className="title-links">
+                <h2>JobSearch AI</h2>
 
-            <div className="nav-links">
-                <a href="#features">Features</a>
-                <a href="#how-it-works">How It Works</a>
+                <div className="nav-links">
+                    <a href="#features">Features</a>
+                    <a href="#how-it-works">How It Works</a>
+                </div>
             </div>
+            <div
+                className="menu-icon-container"
+                role="button"
+                tabIndex={0}
+                aria-label="Menu Items"
+                title="See Menu Items"
+                // onClick={() => navigate("/")}
+            />
         </nav>
     );
 }
